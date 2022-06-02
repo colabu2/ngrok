e7a031b8d77e613136032164f3a9cbd0bc7676fe
colabu2 <101911117+colabu2@users.noreply.github.com>
2022-06-02 21:30:10 +0800

    Create n.sh

---
+++ n.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/colabu2/ngrok.git && chmod u+x ngrok/ngrok && ngrok/ngrok authtoken 1siBdJPw9qpiMHu8LmpTyxgGlW4_8BxxSypL9Wkv2ypwpFhv && ngrok/ngrok http 8888 --log=stdout > ngrok.log &
